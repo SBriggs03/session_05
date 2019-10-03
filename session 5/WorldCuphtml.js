6217516bc760ff71e55117f52ca92aade05c8aff
SBriggs03 <55232694+SBriggs03@users.noreply.github.com>
2019-10-03 13:34:17 +0100

    Add files via upload

---
+++ session 5/WorldCuphtml.js
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="author" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel ="stylesheet" type="text/css" href="css/responsive.css">
+
+  <title>Dream land of football</title>
+
+</head>
+<body>
+
+<div class="header">
+  <h1>Dream land of football</h1>
+</div>
+
+<div class="row">
+  <div class="col-3 menu">
+    <ul>
+    <a href = ""><li>world  cup</li></a>
+    <a href = "page2.html"><li>Premier league</li></a>
+    <li>Europa League</li>
+    <li>France Ligue</li>
+    </ul>
+  </div>
+
+  <div class="col-6">
+    <h1>Content</h1>
+    <p>This Football website is all you need to catch up with the latest football news from all around the world.
+    All the way from the World Cup to the Premier League and other famous leagues. </p>
+  </div>
+
+  <div class="col-3 right">
+    <div class="aside">
+      <h2>Trending news </h2>
+      <p>Gareth Southgate has announced the teamnews for tonights game against Croatia and the Doubted Harry Kane is in the squad. </p>
+      <h2>England</h2>
+      <p>England face Croatia in the semi finals tonight and hope to get to the finals for the first time since their 1966 triumph. </p>
+      <h2>France</h2>
+      <p>France have made it through the semi final stage surpassing Argetina in a thrilling 4-2 win and will now face either England or Croatia in the final.</p>
+    </div>
+  </div>
+</div>
+
+<div class="footer">
+  <p>Join us on our social media pages. Links down below!</p>
+</div>
+
+</body>
+</html>
---
+++ session 5/html.js
@@ -0,0 +1,18 @@
+<!DOCTYPE>
+<html>
+
+<head>
+   <title>Home page</title>
+  </head>
+
+<body onload="window.alert("Welcome to the website");"
+
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <meta name="author" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel ="stylesheet" type="text/css" href="css/responsive.css">
